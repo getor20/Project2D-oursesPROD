b704155e7874eb84bd9960c0efe75f177c3462bc
getor20 <ggetor20@gmail.com>
2025-06-13 00:27:01 +0200

    1

--- Assets/Prefab/Player/PlayerScript.cs
+++ Assets/Prefab/Player/PlayerScript.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    private Rigidbody2D rd2D => GetComponent<Rigidbody2D>();    
+    private Vector2 vector2 = Vector2.zero;
+
+    [SerializeField]
+    private float speed = 10f;
+
+    private void FixedUpdate()
+    {
+        rd2D.velocity = new Vector2(vector2.x * speed, vector2.y * speed);
+    }
     
+    public void Muve(InputAction.CallbackContext context)
+    {
+        vector2 = context.ReadValue<Vector2>();
+    }
+
 }
