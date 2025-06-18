1b79344005f72d0591a4e19e17cf4e6a20e83687
getor20 <ggetor20@gmail.com>
2025-06-18 16:50:33 +0200

    Homework#2

--- Assets/Script/Player/PlayerScript.cs
+++ Assets/Script/Player/PlayerScript.cs
@@ -8,19 +8,19 @@ public class PlayerScript : MonoBehaviour
 
     [SerializeField]
     private float speed = 10f;
+    private bool Flipi = false;
 
     private void FixedUpdate()
     {
         rd2D.velocity = new Vector2(vector2.x * speed, vector2.y * speed);
-    }
-    
-    public void Muve(InputAction.CallbackContext context)
-    {
         if ((vector2.x > 0 && transform.localScale.x < 0) || (vector2.x < 0 && transform.localScale.x > 0))
         {
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
         }
-
+    }
+    
+    public void Muve(InputAction.CallbackContext context)
+    {
         vector2 = context.ReadValue<Vector2>();
     }
 
