0a797005c63f99b86d404dd053a9cb1b1133ae96
getor20 <ggetor20@gmail.com>
2025-07-26 13:12:26 +0200

    1

--- Assets/Script/Player/PlayerAnimator.cs
+++ Assets/Script/Player/PlayerAnimator.cs
@@ -1,6 +1,20 @@
 using Assets.Script.Player;
 using UnityEngine;
 
+namespace Assets.Script.Player
+{
+    internal class PlayerAnimName
+    {
+        public const string DirectionX = "DirectionX";
+        public const string DirectionY = "DirectionY";
+        public const string IsMainSpeed = "IsMainSpeed";
+        public const string AngularDirectionX = "AngularDirectionX";
+        public const string AngularDirectionY = "AngularDirectionY";
+        public const string NewFloat = "New Float";
+        public const string NewFloat0 = "New Float 0";
+    }
+}
+
 public class PlayerAnimator : MonoBehaviour
 {
     private Animator _animator;
