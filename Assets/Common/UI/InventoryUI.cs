e5e67294d53296be13fc5284ef31ea0a523346f5
getor20 <ggetor20@gmail.com>
2025-09-20 13:16:44 +0200

    1

---
+++ Assets/Common/UI/InventoryUI.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField] private Transform _slotParent;
+    [SerializeField] private GameObject _slotPrefab;
+    
+    private InventorySlot[] _slots;
+        
+}
